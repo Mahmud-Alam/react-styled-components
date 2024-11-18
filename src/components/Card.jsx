99a9555646afad57ae9e50ef070a5254567f3b7b
Mahmud-Alam <official.mahmud23@gmail.com>
2024-11-18 15:43:49 +0600

    Create Custom Component and Create Styled Component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,25 @@
 import nerdImage from '../assets/images/illustration.png';
 import Button from './Button';
 import Content from './Content';
-import Image from './Image';
+// import Image from './Image';
 // import Tag from './Tag';
 import {CardContainer, ContentContainer, ButtonContainer} from './styles/Container.styles';
-import {Tag, H1, P} from './styles/Elements';
+import {Tag, H1, P, Image} from './styles/Elements.styles';
+import { StyledTitle } from './styles/Custom.styles';
 
 export default function Card() {
   return (
     <CardContainer>
       <ContentContainer>
         <Tag color="#4361ee">Exclusive</Tag>
-        <H1>React Styled Components</H1>
+        <H1><StyledTitle text="React Styled Components" color="#fff"/></H1>
         <P>Exclusive React JS Tutorial on Styled Components where you will learn why we need this & how to use it.</P>
         <ButtonContainer>
           <Button link="https://lwsbd.link/rsc" text="Watch now" />
           <Button link="https://lwsbd.link/react" text="Github-repo" />
         </ButtonContainer>
       </ContentContainer>
-      <Image ingSrc={nerdImage} altTag="Nerd" width="300px"/>
+      <Image src={nerdImage} alt="Nerd" width="300px"/>
     </CardContainer>
   );
 };
