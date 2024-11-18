20852552a1ca343414ffdd985a20b1fa787fde5d
Mahmud-Alam <official.mahmud23@gmail.com>
2024-11-18 11:10:49 +0600

    update components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import nerdImage from '../assets/images/illustration.png';
+import Button from './Button';
+import Content from './Content';
+import Image from './Image';
+import Tag from './Tag';
+import {CardContainer} from './styles/Container.styles';
 
 export default function Card() {
   return (
-    <div>Card</div>
-  )
-}
+    <CardContainer>
+      <Content>
+        <Tag text="EXCLUSIVE" />
+        <h1>React Styled Components</h1>
+        <p>Exclusive React JS Tutorial on Styled Components where you will learn why we need this & how to use it.</p>
+        <div className="buttons">
+          <Button link="https://lwsbd.link/rsc" text="Watch now" />
+          <Button link="https://lwsbd.link/react" text="Github-repo" />
+        </div>
+      </Content>
+      <Image ingSrc={nerdImage} altTag="Nerd" width="300px"/>
+    </CardContainer>
+  );
+};
---
+++ src/components/styles/Container.styles.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "Montserrat", sans-serif;
+  background: #262626;
+  width: 100%;
+  height: 100vh;
+`;
+
+export const CardContainer = styled.div`
+  position: relative;
+  background: #1e1e1e;
+  height: 350px;
+  width: 550px;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+`;
